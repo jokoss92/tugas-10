b0c68facd84807d93bcad37b87c33f878af392cf
Joko Sarjono S <johno.smakaduta@gmail.com>
2018-09-09 20:52:15 +0700

    Tugas 10

---
+++ script.js
@@ -0,0 +1,8 @@
+const nama = prompt('Siapa namamu?')
+    if(nama == "Joko"){
+        alert("Selamat datang bosque!")
+    } else if (nama == "Doni") {
+        alert("Selamat datang Bro,")
+    } else {
+        alert('Oops, ')
+    }
